b99737383354105bda9f34891b110c4a6a7a7cfe
Danny Lockard <danielhlockard@gmail.com>
2021-03-12 23:51:33 +0000

    Change to allow multiple lines, and reading in files

--- cmd/pglint-cli/main.go
+++ cmd/pglint-cli/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/jonfriesen/pglint/pkg/ecpg"
@@ -13,6 +15,7 @@ func main() {
 	addSemicolons := flag.Bool("addSemicolons", true, "Automatically add semicolons to queries if not already present (will fail without a semicolon)")
 	trimWhitespace := flag.Bool("trim", true, "Trim whitespace at head and tail of query")
 	allowQuestionMarks := flag.Bool("questionmarks", true, "Determines whether or not the to fill question mark placeholders with sample data for linting")
+	inputFile := flag.String("input", "", "What file to read in to analyze, just reads from end of command if empty")
 
 	flag.Parse()
 
@@ -26,12 +29,25 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 	}
+	query := ""
+	inputFileString := *inputFile
+	if inputFileString == "" {
+		query = strings.Join(flag.Args(), " ")
+	} else {
+		file, err := ioutil.ReadFile(inputFileString)
+		if err != nil {
+			fmt.Printf("ERROR: %s", err)
+			os.Exit(1)
+		}
+		query = string(file)
+	}
 
-	query := strings.Join(flag.Args(), " ")
-
-	err = linter.CheckStatement(query)
-	if err != nil {
-		fmt.Println(err)
+	errArray := linter.CheckStatement(query)
+	if len(errArray) != 0 {
+		for _, err := range errArray {
+			fmt.Printf("ERROR: %s", err)
+		}
+		os.Exit(1)
 	}
 
 }
